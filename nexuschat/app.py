e57313eeb6140565febf5aae507a75ca9cb2aa9a
sergiixdd-netizen <sergiixdd@gmail.com>
2026-03-12 20:50:36 +0100

    Update app.py

--- nexuschat/app.py
+++ nexuschat/app.py
@@ -456,7 +456,8 @@ def on_typing(data):
 
 @socketio.on('disconnect')
 def on_disconnect(): pass
-
+with app.app_context():
+    db.create_all()
 if __name__ == '__main__':
     with app.app_context(): db.create_all()
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=False)
